a739a08a10b00ceb399ff58deb861ee66fdd2c2e
Plotnost <zoo1102@mail.ru>
2024-10-25 18:15:28 +0500

    Добавил картинку в карточку товара в Catalog.vue

--- src/components/Catalog.vue
+++ src/components/Catalog.vue
@@ -4,6 +4,7 @@
     <h2>Каталог товаров</h2>
     <div class="product-list">
       <div v-for="product in products" :key="product.id" class="product-card">
+        <img src="./image.png">
         <a href="#/product"class="product-name">{{ product.name }}</a>
         <p class="product-price">{{ product.price }} ₽</p>
         <button @click="addToCart(product)" class="add-to-cart-button">
@@ -16,6 +17,7 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+
 
 interface Product {
   id: number;
@@ -54,6 +56,7 @@ export default defineComponent({
   border: 1px solid #ddd;
   border-radius: 8px;
   padding: 16px;
+  height: 300px;
   width: 200px;
   box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
   display: flex;
@@ -61,6 +64,13 @@ export default defineComponent({
   align-items: center;
   transition: transform 0.2s ease;
   background-color: #fff;
+}
+
+.product-card img{
+  height: 50%;
+  width: 80%;
+  border: 1px solid #000;
+  border-radius: 10px;
 }
 
 .product-card:hover {
